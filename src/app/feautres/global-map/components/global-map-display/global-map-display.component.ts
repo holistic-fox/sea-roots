ab1ade250bf886ba14747a34654caddda55e508c
Michał Witek <michal.witek.katowice@gmail.com>
2024-07-22 17:26:11 +0200

    init @syncfusion map

--- src/app/feautres/global-map/components/global-map-display/global-map-display.component.ts
+++ src/app/feautres/global-map/components/global-map-display/global-map-display.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MapsModule} from "@syncfusion/ej2-angular-maps";
+import * as worldMap from "../../models/world-map.json";
 
 @Component({
   selector: 'marcura-global-map-display',
   standalone: true,
-  imports: [],
+  imports: [MapsModule],
   templateUrl: './global-map-display.component.html',
   styleUrl: './global-map-display.component.scss'
 })
 export class GlobalMapDisplayComponent {
-
+  layers = [{
+    shapeData: worldMap
+  }]
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLicense } from '@syncfusion/ej2-base'
 
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCdkx0RHxbf1x0ZFRHal1QTnRWUiweQnxTdEFjXn1ccXVURGFdWUN+Vw==');
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
