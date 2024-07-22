8929e5370898d592c21d983b7868cfb9a1d40535
Michał Witek <michal.witek.katowice@gmail.com>
2024-07-22 17:39:56 +0200

    navigationLineSettings example

--- src/app/feautres/global-map/components/global-map-display/global-map-display.component.ts
+++ src/app/feautres/global-map/components/global-map-display/global-map-display.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {MapsModule} from "@syncfusion/ej2-angular-maps";
+import {Maps, MapsModule, MapsTooltip, NavigationLine} from "@syncfusion/ej2-angular-maps";
 import * as worldMap from "../../models/world-map.json";
+
+Maps.Inject(MapsTooltip, NavigationLine);
 
 @Component({
   selector: 'marcura-global-map-display',
@@ -10,7 +12,15 @@ import * as worldMap from "../../models/world-map.json";
   styleUrl: './global-map-display.component.scss'
 })
 export class GlobalMapDisplayComponent {
+
   layers = [{
-    shapeData: worldMap
+    shapeData: worldMap,
+    navigationLineSettings: [{
+      visible: true,
+      latitude: [37.772, -122.214],
+      longitude: [-122.214, -122.214],
+      color: 'red',
+      width: 1
+    }]
   }]
 }
